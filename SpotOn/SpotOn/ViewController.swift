07c5bf7da5bc0797a89ed14898784a03191238ce
radhajain <jainr@stanford.edu>
2018-05-17 17:12:49 -0700

    XCode

---
+++ SpotOn/SpotOn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotOn
+//
+//  Created by Radha Jain on 5/17/18.
+//  Copyright © 2018 Radha Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
